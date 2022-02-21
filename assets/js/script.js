bca2a6bef57fe8616702aa6ba15c7eca26312ab8
William <william.contacto@gmail.com>
2022-02-21 20:30:04 -0300

    instruccion if

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//console.log(5 == 5);{
+ //   console.log('Esto es una prueba')
+//}
+var datoUno = 4;
+var datoDos = 5;
+console.log('verificando el valor de la prueba logica: ${datoUno != datoDos}');
+
+if(datoUno != datoDos){
+    console.log('este se ejecuta siempre y cuando la condicion se cumpla'); 
+
+}
+console.log('esto se ejecuta despues del if')
+
+//console.log(5 == 5);
+
